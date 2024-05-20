109b809245dcbd399f5c9ca6fdb846ad4a00b8e8
Dawe129 <161817982+Dawe129@users.noreply.github.com>
2024-05-20 14:32:25 +0200

    Pridani metody pro upravu 6 indexu na desetine stupne

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        String cesta = "C:\\Users\\Honza\\IdeaProjects\\zadani_1\\aktivace_dm_jaro_2024.csv";
+        String cesta = "C:\\Users\\Honza\\IdeaProjects\\zadani_1_03\\aktivace_dm_jaro_2024.csv";
         List<String[]> data = PrectiCSV.CSV(cesta);
 
         for (String[] radek : data) {
--- src/PrectiCSV.java
+++ src/PrectiCSV.java
@@ -9,6 +9,7 @@ public class PrectiCSV {
     public static List<String[]> CSV(String cesta) {
         List<String[]> data = new ArrayList<>();
         String radek;
+        int index[];
         String oddelovani = ",";
 
         try (BufferedReader reader = new BufferedReader(new FileReader(cesta))) {
@@ -20,5 +21,29 @@ public class PrectiCSV {
             e.printStackTrace();
         }
         return data;
+    }
+
+    public static void upravRadek(List<String[]> data, int indexRadku) {
+        if (indexRadku >= 0 && indexRadku < data.size()) {
+            String[] radek = data.get(indexRadku);
+            if (radek.length > 6) {
+                String[] koordinaty = radek[6].split("[°'\"]");
+                try {
+                    double stupne = Double.parseDouble(koordinaty[0]);
+                    double minuty = Double.parseDouble(koordinaty[1]);
+                    double sekundy = Double.parseDouble(koordinaty[2]);
+
+                    double desetinneStupne = stupne + (minuty / 60.0) + (sekundy / 3600.0);
+                    radek[6] = String.valueOf(desetinneStupne);
+                } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+                    System.err.println("Chyba při parsování koordinát.");
+                    e.printStackTrace();
+                }
+            } else {
+                System.err.println("Řádek nemá dostatek prvků.");
+            }
+        } else {
+            System.err.println("Chyba: Index řádku mimo rozsah.");
+        }
     }
 }
