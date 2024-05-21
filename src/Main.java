addf3d8c0e6bf47fea6d86b5282b004ad37d0e33
Dawe129 <161817982+Dawe129@users.noreply.github.com>
2024-05-21 14:17:11 +0200

    Opraveny vypis souboru + zjisteni gps jestli se nachazi na indexu 6

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,14 @@ public class Main {
 
         String cesta = "C:\\Users\\Honza\\IdeaProjects\\zadani_1_03\\aktivace_dm_jaro_2024_original.csv";
         List<String[]> data = PrectiCSV.CSV(cesta);
-        PrectiCSV csv = new PrectiCSV();
 
-        for (String[] radek : data) {
-            for (String hodnota : radek) {
-                System.out.print(hodnota + " ");
-                System.out.println();
+        for (int i = 0; i < data.size(); i++) {
+            try {
+                System.out.println("=======================================");
+                PrectiCSV.upravRadek(data, i);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Chyba u řádku " + i + ": " + e.getMessage());
             }
-            System.out.println();
         }
     }
 }
--- src/PrectiCSV.java
+++ src/PrectiCSV.java
@@ -9,12 +9,11 @@ public class PrectiCSV {
     public static List<String[]> CSV(String cesta) {
         List<String[]> data = new ArrayList<>();
         String radek;
-        int index[];
-        String oddelovani = ",";
+        String oddelovac = ";";
 
         try (BufferedReader reader = new BufferedReader(new FileReader(cesta))) {
             while ((radek = reader.readLine()) != null) {
-                String[] hodnoty = radek.split(oddelovani);
+                String[] hodnoty = radek.split(oddelovac);
                 data.add(hodnoty);
             }
         } catch (IOException e) {
@@ -26,24 +25,35 @@ public class PrectiCSV {
     public static void upravRadek(List<String[]> data, int indexRadku) {
         if (indexRadku >= 0 && indexRadku < data.size()) {
             String[] radek = data.get(indexRadku);
-            if (radek.length > 6) {
-                String[] koordinaty = radek[6].split("[/(\\d{2}\\°\\d{1,2}\\'\\d{1,2}\\.\\d{3}\\\"\\w{1})\\s(\\d{2}\\°\\d{1,2}\\'\\d{1,2}\\.\\d{3}\\\"\\w{1})/gm]");
-                try {
-                    double stupne = Double.parseDouble(koordinaty[0]);
-                    double minuty = Double.parseDouble(koordinaty[1]);
-                    double sekundy = Double.parseDouble(koordinaty[2]);
+            if (radek != null) {
+                System.out.println("Informace z řádku:");
+                for (String hodnota : radek) {
+                    System.out.println(hodnota);
+                }
+                if (radek.length > 6 && radek[6] != null && !radek[6].isEmpty()) {
+                    try {
+                        String[] koordinaty = radek[6].split("[/(\\d{2}\\°\\d{1,2}\\'\\d{1,2}\\.\\d{3}\\\"\\w{1})\\s(\\d{2}\\°\\d{1,2}\\'\\d{1,2}\\.\\d{3}\\\"\\w{1})/gm]");
+                        if (koordinaty.length >= 3) {
+                            double stupne = Double.parseDouble(koordinaty[0]);
+                            double minuty = Double.parseDouble(koordinaty[1]);
+                            double sekundy = Double.parseDouble(koordinaty[2]);
 
-                    double desetinneStupne = stupne + (minuty / 60.0) + (sekundy / 3600.0);
-                    radek[6] = String.valueOf(desetinneStupne);
-                } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
-                    System.out.println("Nelze upravit");
-                    e.printStackTrace();
+                            double desetinneStupne = stupne + (minuty / 60.0) + (sekundy / 3600.0);
+                            System.out.println("Souřadnice úspěšně upraveny: " + desetinneStupne);
+                        } else {
+                            System.out.println("Špatný formát souřadnic.");
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println("Chyba při převodu souřadnic: " + e.getMessage());
+                    }
+                } else {
+                    System.out.println("Tento řádek nemá uvedenou GPS.");
                 }
             } else {
-                System.out.println("Tento uzivatel nema uvedenou gps");
+                System.out.println("Řádek není definován.");
             }
         } else {
-            System.out.println("Uzivatel neni uvedeny");
+            System.out.println("Uživatel není uveden.");
         }
     }
 }
